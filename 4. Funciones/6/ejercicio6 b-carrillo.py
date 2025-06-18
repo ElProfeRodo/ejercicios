ed708d3754c6cb70cd2c2aa0351f32c2531cb15e
BenjaminCarrillo <ben.carrillo@duocuc.cl>
2025-06-18 09:33:06 -0400

    Update ejercicio6 b-carrillo.py

--- 4. Funciones/6/ejercicio6 b-carrillo.py
+++ 4. Funciones/6/ejercicio6 b-carrillo.py
@@ -8,6 +8,8 @@ def operaciones(num1,num2):
         print(f"{num1}-{num2}= {resta}")
         print(f"{num1}x{num2}= {multi}")
         print(f"{num1}/{num2}= {divid}")
+    else: 
+        print("ingrese un numero diferente a cero")
 num1= int(input("ingresa el primer numero= "))
 num2= int(input("ingresa el segundo numero= "))
 operaciones(num1,num2)
