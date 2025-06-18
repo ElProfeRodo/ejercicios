a2f2174bad94e113b24dd6222362faf392590f64
BenjaminCarrillo <ben.carrillo@duocuc.cl>
2025-06-18 09:29:00 -0400

    Update ejercicio4 b-carrillo.py

--- 4. Funciones/4/ejercicio4 b-carrillo.py
+++ 4. Funciones/4/ejercicio4 b-carrillo.py
@@ -1,6 +1,6 @@
-def area():
-    altura= int(input("ingresa la altura= "))
-    ancho= int(input("ingresa el ancho= "))
+def area(altura, ancho):
     area=altura*ancho
     print(f"el area es = {area}")
-area()
+altura= int(input("ingresa la altura= "))
+ancho= int(input("ingresa el ancho= "))
+area(altura, ancho)
