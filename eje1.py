d97ca734369ac22563475efc5ab98ab630fa0cd4
nase20 <phillips.1999@hotmail.com>
2025-06-18 09:35:19 -0400

    Delete eje1.py

--- eje1.py
+++
@@ -1,8 +0,0 @@
-def suma():
-    numeros=[]
-    for i in range(15):
-        num=int(input("ingrese un numero: "))
-        numeros.append(num)
-    suma=sum(numeros)
-    print(f"la suma de los numeros es:{suma} ")
-suma()
