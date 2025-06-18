fee49bdd52448433f2d9f74b7a9f5c1a7cdbb8ce
Maxwell-RT <claud.munozs@duocuc.cl>
2025-06-18 09:12:11 -0400

    Add files via upload

---
+++ 4. Funciones/3/correo Claudio.py
@@ -0,0 +1,6 @@
+def mail():
+    nombre= input("Ingrese su nombre: ")
+    apellido= input("Ingrese su apellido: ")
+    correo=("gmail.com")
+    print("Su correo es: " + nombre + "." + apellido + "@" + correo)
+mail()
