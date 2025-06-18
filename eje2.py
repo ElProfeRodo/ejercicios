7d50b334d1c27f7792ed2151c6ce4ae0d52ab514
nase20 <phillips.1999@hotmail.com>
2025-06-18 09:35:06 -0400

    Delete eje2.py

--- eje2.py
+++
@@ -1,9 +0,0 @@
-def años():
-    edad=int(input("ingrese su edad: "))
-    while edad>=18:
-        print("eres mayor de edad")
-        break
-    else:
-        print("eres menor de edad")
-        
-años()
