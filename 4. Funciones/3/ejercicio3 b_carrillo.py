002b3cfc46b047533852055780a123a1bfbdbccb
BenjaminCarrillo <ben.carrillo@duocuc.cl>
2025-06-18 09:22:59 -0400

    Update ejercicio3 b_carrillo.py

--- 4. Funciones/3/ejercicio3 b_carrillo.py
+++ 4. Funciones/3/ejercicio3 b_carrillo.py
@@ -1,5 +1,5 @@
 def correo():
   nombre= input("ingresa tu nombre")
   apellido= input("ingresa tu apellido")
-  print(f"{nombre}.{apellido}@gmail.com")
+  print(f"su correo es= {nombre}.{apellido}@gmail.com")
 correo()
