062f8202ff005b8ea6fc56207cacfc82adc79751
nase20 <phillips.1999@hotmail.com>
2025-06-18 09:37:42 -0400

    Add files via upload

---
+++ 4. Funciones/3/ej3felipe.py
@@ -0,0 +1,7 @@
+def user(nomb,apell):
+    nombre=input("ingrese su nombre: ")
+    apellido=input("ingrese su apellido: ")
+    usuario=nombre+"."+apellido+"@gmail.com"
+    print(usuario)
+user()
+    
