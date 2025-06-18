07d1ca922f119707dc83a55dd9666eb887c1b40e
Maxwell-RT <claud.munozs@duocuc.cl>
2025-06-18 09:35:29 -0400

    Add files via upload

---
+++ 4. Funciones/4/Area Rectangulo.py
@@ -0,0 +1,8 @@
+def rectangulo(altura, base):
+
+    Area=(base*altura)
+    return Area
+
+altura=float(input("Ingresar altura: "))
+base=float(input("Ingresar base: "))
+print(rectangulo (altura,base), "Centimetros cuadrados")
