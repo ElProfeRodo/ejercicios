31a801e1357b6e6dc074e29b65ee39d008a7fccb
nase20 <phillips.1999@hotmail.com>
2025-06-18 09:34:54 -0400

    Delete eje3.py

--- eje3.py
+++
@@ -1,7 +0,0 @@
-def user(nomb,apell):
-    nombre=input("ingrese su nombre: ")
-    apellido=input("ingrese su apellido: ")
-    usuario=nombre+"."+apellido+"@gmail.com"
-    print(usuario)
-user()
-    
