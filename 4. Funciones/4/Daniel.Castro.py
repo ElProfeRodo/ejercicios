bf09007b45947ae1b43010de0c2b83396b8eacad
Dan1elCastroC <danie.castroc@duocuc.cl>
2025-06-18 09:18:45 -0400

    Update Daniel.Castro.py

--- 4. Funciones/4/Daniel.Castro.py
+++ 4. Funciones/4/Daniel.Castro.py
@@ -1,3 +1,8 @@
+# 4. Calcular el área de un rectángulo
 def calcular_area_rectangulo(altura, ancho):
     area = int(altura) * int(ancho)
-    print("El área del rectángulo es:", area, "cm cuadrados")
+    return area
+
+altura = input("Ingrese la altura del rectángulo: ")
+ancho = input("Ingrese el ancho del rectángulo: ")
+print("Ejercicio 4 - Área del rectángulo:", calcular_area_rectangulo(altura, ancho), "cm cuadrados")
