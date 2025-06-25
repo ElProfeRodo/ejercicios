0be3b0806f4d50df51b4be3f5be309e2b02e4e82
florencia240 <flo.mora@duocuc.cl>
2025-06-25 13:49:49 -0400

    Add files via upload

---
+++ 5. Listas/3/#crear lista de colores.py
@@ -0,0 +1,8 @@
+#crear lista de colores
+colores = ["rojo", "verde", "azul"]
+
+#cambiar color "verde" por amarillo"
+colores[colores.index("verde")]="amarillo"
+
+#mostrar lista de colores actualizado
+print(colores)
