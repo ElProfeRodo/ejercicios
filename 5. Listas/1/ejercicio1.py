5e22ffc6a5c7a776c85b237e741481eb8411c792
Sebastian <se.aranedaj@duocuc.cl>
2025-06-25 13:36:05 -0400

    Create ejercicio1.py

---
+++ 5. Listas/1/ejercicio1.py
@@ -0,0 +1,11 @@
+'''
+1. Crear una lista llamada frutas que contenga al menos cinco tipos de frutas. Luego,
+mostrar la lista por pantalla.
+'''
+frutas = ['manzana', 'pepino', 'tomate', 'melon', 'limon']
+for f in frutas:
+    print(f)
+
+frutas = ['manzana', 'pepino', 'tomate', 'melon', 'limon']
+for i in range(len(frutas)):
+    print (frutas[i])
