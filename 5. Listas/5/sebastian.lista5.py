aee4a63ee0d949f2bb76908b397abb11c941d9ed
Sebastian <se.aranedaj@duocuc.cl>
2025-06-25 13:59:01 -0400

    Create sebastian.lista5.py

---
+++ 5. Listas/5/sebastian.lista5.py
@@ -0,0 +1,13 @@
+'''
+5. Dada la lista animales = [“perro”, “gato”, “pájaro”, “pez”], elimina
+“gato” y muestra por pantalla el resultado
+'''
+#con remove
+animales = [ "perro", "gato", "pajaro", "pez"]
+animales.remove("gato")
+print(animales)
+
+#con pop 
+animales = [ "perro", "gato", "pajaro", "pez"]
+animales.pop(1)
+print(animales)
