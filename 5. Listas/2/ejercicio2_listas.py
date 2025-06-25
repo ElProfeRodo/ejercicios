223ba1debe6e83ff39dda6e77c1f9ce032130f6d
Sebastian <se.aranedaj@duocuc.cl>
2025-06-25 13:35:12 -0400

    Create ejercicio2_listas.py

---
+++ 5. Listas/2/ejercicio2_listas.py
@@ -0,0 +1,7 @@
+'''
+2. Dada la lista numeros = [10, 20, 30, 40, 50], mostrar por pantalla el primer
+y el último elemento
+'''
+numero = ['10', '20', '30', '40', '50']
+print (numero[0])
+print(numero[4])
