68d9e70aff65638100dd8ddef979671c757a584b
florencia240 <flo.mora@duocuc.cl>
2025-06-25 13:47:52 -0400

    Add files via upload

---
+++ 5. Listas/5/Untitled-gato.py
@@ -0,0 +1,11 @@
+#crear lista de animales
+animales = ["perro", "gato", "pajaro", "pez"]
+
+#eliminar "gato"
+print(animales)
+#'perro', 'gato', 'pajaro', 'pez'
+
+animales.pop(1)
+
+#mostrar lista de animales actualizada
+print(animales)
