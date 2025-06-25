f123e415674b7df1d912d4561b84c8e575d88761
Sebastian <se.aranedaj@duocuc.cl>
2025-06-25 13:52:13 -0400

    Create sebastian_listas4.py

---
+++ 5. Listas/4/sebastian_listas4.py
@@ -0,0 +1,12 @@
+'''
+4. Crear una lista vacía y solicitar al usuario que ingrese 5 nombres. Agregar cada
+nombre a la lista y luego mostrarla por pantalla
+'''
+nombres = []
+nom1 = input('ingrese un nombre: ')
+nom2 = input('ingrese un nombre: ')
+nom3 = input('ingrese un nombre: ')
+nom4 = input('ingrese un nombre: ')
+nom5 = input('ingrese un nombre: ')
+nombres = [nom1, nom2, nom3, nom4, nom5]
+print(nombres)
