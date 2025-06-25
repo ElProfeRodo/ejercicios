3be4f31309cdda3f6fd44426bde7a4f29d9582e8
Nittram <128756221+Nittram@users.noreply.github.com>
2025-06-25 13:45:11 -0400

    Add files via upload

---
+++ 5. Listas/3/Martin_Diaz3.py
@@ -0,0 +1,10 @@
+colores = ["Rojo", "Verde", "Azul"]
+
+for i in colores:
+    print(f"La lista normal: {i}\n")
+
+# Lista actualizada
+colores[1] = "Amarillo"
+
+for i in colores:
+    print(f"La lista actualizada: {i}\n")
