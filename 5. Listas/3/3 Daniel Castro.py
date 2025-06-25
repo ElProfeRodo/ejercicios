c6a61f06d258c94fc8b86014c4782986c721f66e
Dan1elCastroC <danie.castroc@duocuc.cl>
2025-06-25 13:51:43 -0400

    Delete 5. Listas/3/3 Daniel Castro.py

--- 5. Listas/3/3 Daniel Castro.py
+++
@@ -1,4 +0,0 @@
-#3 Crear una lista con los días de la semana y mostrar los días de lunes a viernes en un ciclo.
-semana=["lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
-for i in semana:
-    print(i)
