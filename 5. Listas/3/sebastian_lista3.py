1ab249fb9d569c00726bf68a9b37f3124b5e2576
Sebastian <se.aranedaj@duocuc.cl>
2025-06-25 13:41:55 -0400

    Create

---
+++ 5. Listas/3/sebastian_lista3.py
@@ -0,0 +1,9 @@
+'''
+3. Crear una lista llamada colores con los valores “rojo”, “verde” y “azul”.
+Cambiar el color “verde” por “amarillo” y mostrar la lista actualizada por
+pantalla
+'''
+colores = ['rojo', 'verde', 'azul']
+print(colores)
+colores[1] = 'amarillo'
+print(colores)
