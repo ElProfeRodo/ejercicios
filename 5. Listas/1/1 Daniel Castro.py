cb348bb864536902ca3f30122d6d015908bdd735
Dan1elCastroC <danie.castroc@duocuc.cl>
2025-06-25 13:20:35 -0400

    Create 1 Daniel Castro.py

---
+++ 5. Listas/1/1 Daniel Castro.py
@@ -0,0 +1,4 @@
+#1 Crear una lista llamada frutas que contenga al menos cinco tipos de frutas. Luego, mostrar la lista por pantalla.
+frutas = ["mandarina", "pera", "naranja", "platano", "manzana"]
+for fruta in frutas:
+    print(fruta)
