0a93427bf0255a5200237e3b5be69ad814f1bda0
El Profe Rodo <elproferodofer@gmail.com>
2025-07-02 09:48:12 -0400

    Update aventureros.py

--- aventureros.py
+++ aventureros.py
@@ -1,44 +1,52 @@
-aventureros = {}
+listado_av = {}
 
 """
-aventureros = {
-    'AV123': {
-        'nombre': 'Kevin',
-        'edad': 20,
-        'puntaje': [100, 512]
+listado_av = {
+    'A001': {
+        'nombre': 'Alejandro',
+        'edad': 30,
+        'puntajes': []
     },
-
-    'AV456': {
-        'nombre': 'Marcela',
-        'edad': 33,
-        'puntaje': []
+    'A002': {
+        'nombre': 'Alejandro',
+        'edad': 30,
+        'puntajes': []
+    },
+    'A003': {
+        'nombre': 'Alejandro',
+        'edad': 30,
+        'puntajes': []
     }
 }
 """
 
-def registrar_aventurero(listado_av, codigo_av, nombre_av, edad_av):
-    if codigo_av not in listado_av:
-        listado_av[codigo_av] = {'nombre': nombre_av, 'edad': edad_av, 'puntaje': []}
-        print("Aventurero registrado!")
+def registra_av(aventureros, codigo_av, datos_av):
+    if codigo_av not in aventureros:
+        aventureros[codigo_av] = {
+            'nombre': datos_av[0],
+            'edad': datos_av[1], 
+            'puntajes': []
+        }
+        return True
     else:
-        print("El código ya existe")
+        return False
 
-def agregar_puntaje(listado_av, codigo_av, puntaje):
-    if codigo_av in listado_av:
-        listado_av[codigo_av]['puntaje'].append(puntaje)
-        print("Puntaje agregado")
+def registra_puntaje(aventureros, codigo_av, puntaje):
+    if codigo_av in aventureros:
+        aventureros[codigo_av]['puntajes'].append(puntaje)
+        return True
     else:
-        print("El código no existe")
+        return False
 
 while True:
-    print("1. Registrar aventurero")
-    print("2. Agregar puntaje de sesión")
-    print("3. Modificar puntaje")
-    print("4. Ver participación total y promedio")
-    print("5. Ver aventureros con bajo promedio")
-    print("6. Listar aventureros y puntajes")
-    print("7. Listar aventureros por edad")
-    print("8. SALIR")
+    print("1. Registrar aventureros con su código único, nombre y edad.")
+    print("2. Registrar puntajes obtenidos por sesión para cada aventurero.")
+    print("3. Modificar un puntaje específico de una sesión dada.")
+    print("4. Visualizar el total acumulado y el promedio de puntajes por aventurero.")
+    print("5. Mostrar los aventureros cuyo promedio de puntajes esté por debajo de un umbral dado.")
+    print("6. Listar todos los aventureros con sus datos y puntajes.")
+    print("7. Filtrar los aventureros por edad específica.")
+    print("8. Salir")
 
     opcion = int(input("Opción: "))
 
@@ -46,32 +54,25 @@ while True:
         codigo = input("Código: ")
         nombre = input("Nombre: ")
         edad = int(input("Edad: "))
-        registrar_aventurero(aventureros, codigo, nombre, edad)
-        print(aventureros)
-
+        if registra_av(listado_av, codigo, [nombre, edad]):
+            print("Registro exitoso")
+        else:
+            print("El código ya existe")
     elif opcion == 2:
         codigo = input("Código: ")
         puntaje = int(input("Puntaje: "))
-        agregar_puntaje(aventureros, codigo, puntaje)
-        print(aventureros)
-
+        registra_puntaje(listado_av, codigo, puntaje)
     elif opcion == 3:
         pass
-
     elif opcion == 4:
         pass
-
     elif opcion == 5:
         pass
-
     elif opcion == 6:
         pass
-
     elif opcion == 7:
         pass
-
     elif opcion == 8:
         break
-
     else:
         print("La opción es incorrecta")
